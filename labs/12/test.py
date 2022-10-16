0edfae08e5c5e4b9022cafc48b18cc5758552d45
Alexey Medvedev <45236468+medaldev@users.noreply.github.com>
2022-10-16 14:44:01 +0300

    16 to be continued

--- labs/12/test.py
+++ labs/12/test.py
@@ -23,16 +23,16 @@ def simpson(func, a, b, n):
     if n % 2 != 0:
         raise Exception("n должно быть чётным в ф-ле Симпсона")
     m = n // 2
-    h = (b - a) / n
+    h = (b - a) / (2 * m)
     xs = arange(a, b, h)
     result = 0
 
-    result += func(xs[0]) + func(xs[n])
+    result += func(xs[0]) + func(xs[(2 * m - 1)])
 
-    for i in range(1, n, 2):
+    for i in range(1, (2 * m), 2):
         result += 4 * (func(xs[i]))
 
-    for i in range(2, n - 1, 2):
+    for i in range(2, (2 * m) - 1, 2):
         result += 2 * (func(xs[i]))
 
     result = (h / 3) * result
@@ -53,4 +53,67 @@ def newton(func, a, b, n):
     return result
 
 
+def chebishev(func, A, B, m):
+    ts = [-0.883862, -0.529657, -0.323912, 0, 0.323912, 0.529657, 0.883862]
+    n = len(ts)
+    h = (B - A) / m
+    xs = arange(A, B, h)
 
+    result = 0
+
+    for j in range(m - 1):
+
+        a, b = xs[j], xs[j + 1]
+
+        for i in range(len(ts)):
+            result += func(((b + a) / 2) + ((b - a) / 2) * ts[i])
+
+        result *= ((b - a) / 2)
+
+    return result
+
+
+from methods.msa import MSA
+
+
+def gauss(func, A, B, m, eps):
+    ts = [-0.883862, -0.529657, -0.323912, 0, 0.323912, 0.529657, 0.883862]
+    n = len(ts)
+    h = (B - A) / m
+    xs = arange(A, B, h)
+
+    t_left = [[ts[j] ** i for j in range(n)] for i in range(0, n)]
+    t_right = [(2 / (k + 1)) if k % 2 == 0 else 0 for k in range(0, n)]
+
+    A_coefs = MSA(A=t_left, F=list_to_vector(t_right), X=create_random_vector(n, (1, 5)), eps=eps, max_itera=10000)[0]
+
+    result = 0
+
+    for j in range(m - 1):
+
+        a, b = xs[j], xs[j + 1]
+
+        for i in range(n):
+            result += A_coefs[i][0] * func(((b + a) / 2) + ((b - a) / 2) * ts[i])
+
+        result *= ((b - a) / 2)
+
+    return result
+
+
+def list_to_vector(lst):
+    return [[el] for el in lst]
+
+
+ifunc = lambda x: x * 2
+
+fr = 1
+to = 4
+
+segments = 18
+
+print(trapeze(ifunc, fr, to, segments))
+print(simpson(ifunc, fr, to, segments))
+print(newton(ifunc, fr, to, segments))
+print(chebishev(ifunc, fr, to, segments))
+print(gauss(ifunc, fr, to, segments, 1e-3))
